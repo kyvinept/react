320758dd3338d8d847183dcd82e5be4fd5f6c338
admin <>
2020-09-22 15:41:27 +0300

    Updates

---
+++ precache-manifest.91d503c733262051875c6d74a6b68369.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1af034d8e728c0d7e6d7685989a03c22",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "29d2c6cc9919f975e465",
+    "url": "/react/static/css/main.cabc8f89.chunk.css"
+  },
+  {
+    "revision": "5e742aa4222ec35899c2",
+    "url": "/react/static/js/2.78345cdc.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/react/static/js/2.78345cdc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29d2c6cc9919f975e465",
+    "url": "/react/static/js/main.db4867aa.chunk.js"
+  },
+  {
+    "revision": "10c2fc0778cfd83222f9",
+    "url": "/react/static/js/runtime-main.b7f0fd33.js"
+  },
+  {
+    "revision": "04d9cf2a831f2ec0bbe46ee094f2d5a1",
+    "url": "/react/static/media/availableOnAppStore.04d9cf2a.png"
+  },
+  {
+    "revision": "8d6370c36cb4b18ba3fe77cb72a09e11",
+    "url": "/react/static/media/availableOnGooglePlay.8d6370c3.png"
+  },
+  {
+    "revision": "c69005ddebb36a21d52702f583d7a9f3",
+    "url": "/react/static/media/avatar.c69005dd.png"
+  },
+  {
+    "revision": "80b48b36b9b7d5f68327caeeb768c0d8",
+    "url": "/react/static/media/backgroundWork.80b48b36.jpeg"
+  },
+  {
+    "revision": "0bea4f750268dab941b69e0c7a20292a",
+    "url": "/react/static/media/defaultProjectScreen.0bea4f75.jpg"
+  },
+  {
+    "revision": "5177230eec27e10d67c91d3ab2119e59",
+    "url": "/react/static/media/flag.5177230e.png"
+  },
+  {
+    "revision": "98f33bc771e74ca7d06c3f02660ca888",
+    "url": "/react/static/media/logo.98f33bc7.png"
+  },
+  {
+    "revision": "0b7b36de9a59bf8cc270e405cb3a82c5",
+    "url": "/react/static/media/technique.0b7b36de.png"
+  }
+]);
